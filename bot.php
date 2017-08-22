ff98c5521643ee912789c879d72bb9f512e2fc56
l2okii <sfuangk@celestica.com>
2017-08-22 09:25:59 +0700

    add bot

--- bot.php
+++ bot.php
@@ -17,10 +17,18 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => $text
-			];
+			if ($text == 'auto'){
+				$messages = [
+					'type' => 'text',
+					'text' => 'fuckkkkk'
+				];
+			}
+			else {
+				$messages = [
+					'type' => 'text',
+					'text' => $text
+				];
+			}
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
